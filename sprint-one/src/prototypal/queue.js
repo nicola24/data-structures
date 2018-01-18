ded325a889a85e12f60bc849eb63a47983495aaa
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-18 11:38:49 -0800

    Completed Prototypal Pattern for both stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,7 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  obj.count = 0; 
+  obj.tracker = 0;
+  
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.count + this.tracker] = value;
+    this.count++;
+  },
+  dequeue: function() {
+    if (this.count === 0) {
+      return 0;
+    }
+    var result = this.storage[this.tracker];
+    delete this.storage[this.tracker];
+    this.count--;
+    this.tracker++; 
+    return result;
+  },
+  size: function() {
+    return this.count;
+  }
+   
+};
 
