1250fc6a5b1e23e2f77a7c48d042335dc09732cc
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-18 16:55:15 -0800

    All tests passed for Part 1

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.tracker = 0;
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.storage[this.count + this.tracker] = value;
+    this.count++;  
+  }
+  
+  dequeue() {
+    if (this.count === 0) { 
+      return 0;
+    }
+    var result = this.storage[this.tracker];
+    delete this.storage[this.tracker];
+    this.count--;
+    this.tracker++;
+    return result;
+  }
+  
+  size() {
+    return this.count;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {};
   }
-
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+  
+  pop() {
+    if (this.count === 0) {
+      return 0;
+    }
+    delete this.storage[this.count];
+    this.count--;
+    return this.storage[this.count];
+  }
+  
+  size() {
+    return this.count;
+  }
 }
+  
+var testObj = new Stack(); 
