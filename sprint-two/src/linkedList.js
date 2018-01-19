b4c86412a447a755b0dfe92682c85bc980c4c55c
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-19 12:24:45 -0800

    completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,39 +5,56 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null; 
   
-
+  //takes value and adds it to the end of the list 
   list.addToTail = function(value) {
-    //takes value and adds it to the end of the list 
-    
-    // this is now an object 
+  
+    // create a new Node object 
     var newNode = Node(value); 
-    if (!list.head) { //indicates an empty list
+
+    //indicates an empty list
+    if (!list.head) { 
     
-    //list.head needs to be assigned to this object 
+    //add head to the newNode object
       this.head = newNode; 
     
-      //list.tail needs to be assinged to this object
+      //add tail to the NewNode object
       this.tail = newNode;
-    } else { // if list is not empty
-      // reasigned tail to new node
-      this.tail = newNode;
+
+    // if list is not empty
+    } else { 
+
+      this.tail.next = newNode; //we reassign the next pointer to point to our newest node
+      this.tail = newNode; // we designate our newest node as the tail 
     }  
   };
 
+  //removes the first node and returns its value 
   list.removeHead = function() {
-    //removes the first node and returns its value 
-    if (list.head.next) {
-      var result = list.head;
-      delete list.head;
-    }
-    return result;
+    var result = this.head; //store head in a variable
+    var val = this.head.value; //store value of head in a variable
+    this.head = this.head.next; //reassign the head pointer to next node
+    delete result; // delete the head
+    return val; // return the stored value of head
   };
 
+  //returns boolean reflecting whether or not the linked list 
+  //contains the target
   list.contains = function(target) {
-    //returns boolean reflecting whether or not the linked list 
-    //contains the target
-    
-    
+    //we only can access a linked list via head or tail
+    var node = this.head; 
+    //so we declare a var for head to enter our list 
+    while (node) {
+    //set a conditional that while there is a node in our list  
+      if (node.value === target) {
+      //check if that node's value === target
+        return true;
+        // if it does, return true
+      }
+      node = node.next;
+      // if it doesn't, move on to next node
+    }  
+    return false; 
+    // repeat until true is returned, else return false 
   };
 
   return list;
