5d1892f6e99d09fff06b4d878cef5e10eb5be71d
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-18 16:20:41 -0800

    completed pseudo pattern of queue and statck

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+  this.tracker = 0;  
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count + this.tracker] = value;
+  this.count++; 
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.count === 0) {
+    return 0;
+  }
+  var result = this.storage[this.tracker];
+  delete this.storage[this.count];
+  this.count--;
+  this.tracker++;
+  return result;
+};
+
+Queue.prototype.size = function() {
+  return this.count; 
+};
 
